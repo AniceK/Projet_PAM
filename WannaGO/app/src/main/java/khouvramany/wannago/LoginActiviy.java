0e7134d3cb1c5ff5c9f9abddd157d345382a3bc2
AniceK <a.khomany@gmail.com>
2017-01-07 23:59:25 +0100

    add user and password needed for login

--- WannaGO/app/src/main/java/khouvramany/wannago/LoginActiviy.java
+++ WannaGO/app/src/main/java/khouvramany/wannago/LoginActiviy.java
@@ -24,39 +24,46 @@ public class LoginActiviy extends AppCompatActivity {
         EditText user = (EditText) findViewById(R.id.iduser);
         EditText pwd = (EditText) findViewById(R.id.passworduser);
 
-        userDB.open();
-        User newbee = new User(user.getText().toString(),pwd.getText().toString());
-        User target = userDB.getUserbyName(newbee.getName());
+        if (user.getText().toString().matches("")) {
+            Toast.makeText(this, "Please enter a username", Toast.LENGTH_LONG).show();
+        }else if (pwd.getText().toString().matches("")) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_LONG).show();
+        }else {
 
-        // Create user if not existing
-        if (target.getName() == null){
-            Log.v(TAG,"user :"+user.getText().toString()+" not retrieved in database");
+            userDB.open();
+            User newbee = new User(user.getText().toString(), pwd.getText().toString());
+            User target = userDB.getUserbyName(newbee.getName());
 
-            userDB.insertUser(newbee);
-            Toast.makeText(this,"New user created",Toast.LENGTH_LONG).show();
+            // Create user if not existing
+            if (target.getName() == null) {
+                Log.v(TAG, "user :" + user.getText().toString() + " not retrieved in database");
+
+                userDB.insertUser(newbee);
+                Toast.makeText(this, "New user created", Toast.LENGTH_LONG).show();
 
 
-            Intent prerun = new Intent(this, PreRunActivity.class);
-            prerun.putExtra("user", user.getText().toString());
-            startActivity(prerun);
-        } else {
-            Log.v(TAG,"user :"+target.toString()+" found in database");
-            // Refuse access if password is not valid
-            if ( !target.getPassword().equals(newbee.getPassword()) )   {
-                Log.v(TAG,"user :"+user.getText().toString()+" --  invalid password");
-                Log.v(TAG,"user :"+user.getText().toString()+" --  TARGET           NEWBE");
-                Log.v(TAG,"user :"+user.getText().toString()+" --  "+target.getPassword()+"     !=      "+newbee.getPassword());
-                // Display the error on screen
-
-                Toast.makeText(this,"Invalid password",Toast.LENGTH_LONG).show();
-
-            } else {
                 Intent prerun = new Intent(this, PreRunActivity.class);
-                prerun.putExtra("user", newbee.getName());
+                prerun.putExtra("user", user.getText().toString());
                 startActivity(prerun);
+            } else {
+                Log.v(TAG, "user :" + target.toString() + " found in database");
+                // Refuse access if password is not valid
+                if (!target.getPassword().equals(newbee.getPassword())) {
+                    Log.v(TAG, "user :" + user.getText().toString() + " --  invalid password");
+                    Log.v(TAG, "user :" + user.getText().toString() + " --  TARGET           NEWBE");
+                    Log.v(TAG, "user :" + user.getText().toString() + " --  " + target.getPassword() + "     !=      " + newbee.getPassword());
+                    // Display the error on screen
+
+                    Toast.makeText(this, "Invalid password", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Intent prerun = new Intent(this, PreRunActivity.class);
+                    prerun.putExtra("user", newbee.getName());
+                    startActivity(prerun);
+                }
             }
+            userDB.close();
         }
-        userDB.close();
     }
 
     public boolean checkCredentials(String name, String password) {
